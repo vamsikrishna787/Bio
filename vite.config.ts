29690c9dcbfd13cdff935caf19a55f8e23387c93
Vamsi Bol <vamsikrishna787@gmail.com>
2026-09-15 19:16:11 -0500

    ss

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,5 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({ jsxRuntime: 'classic' })],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react-dom/client'],
-      output: {
-        format: 'iife',
-        entryFileNames: 'assets/index-[hash].js',
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'react-dom/client': 'ReactDOM',
-        },
-      },
-    },
-  },
+  plugins: [react()],
 })
